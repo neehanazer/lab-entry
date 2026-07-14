af6fca5b4450b5f04a315e56c312893b2c6543a8
neehanazer <neehanaz226@gmail.com>
2026-07-14 14:32:09 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import AddLab from './components/Addlab'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+      <BrowserRouter>
+    <Routes>
+  <Route path="/" element={<AddLab />} />
+  {/* <Route path="view" element={<ViewDonor />} /> */}
+</Routes>
+     </BrowserRouter>
     </>
   )
 }
